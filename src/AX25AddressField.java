96734af8e8bd3097f6c8b9938fec6e310c07553f
Anoop <anoopvkm@gmail.com>
2014-03-09 15:29:30 +0530

    Address field class finished. started AX.25 Frame class

--- src/AX25AddressField.java
+++ src/AX25AddressField.java
@@ -11,6 +11,9 @@ public class AX25AddressField {
 	// Secondary Station Identifier
 	public byte Ssid;
 	
+	
+	// SSID Mask
+    private static final byte SSIDMask = 0x60;
 	/**
 	 * AX.25 Adrress Field constructor 
 	 */
@@ -29,6 +32,60 @@ public class AX25AddressField {
 		this.Ssid = ssid;
 	}
 	
+	/**
+	 * AX.25 Adrress Field constructor to construct from a byte array
+	 * @param framePart byte array of the frame
+	 */
+	public AX25AddressField(byte[] framePart){
+		this(framePart,0);
+	}
+	
+	/**
+	 * AX.25 Adrress Field constructor to construct from a byte array and an offset
+	 * @param framePart byte array of the frame
+	 * @param offset the point to start with 
+	 */
+	public AX25AddressField(byte[] framePart,int offset){
+		
+		// Call Sign
+		char[] tmp = new char[6];
+		
+		tmp[0] = (char)(framePart[offset] >> 1);
+		tmp[1] = (char)(framePart[offset + 1] >> 1);
+		tmp[2] = (char)(framePart[offset + 2] >> 1);
+		tmp[3] = (char)(framePart[offset + 3] >> 1);
+		tmp[4] = (char)(framePart[offset + 4] >> 1);
+		tmp[5] = (char)(framePart[offset + 5] >> 1);
+		this.CallSign = new String(tmp);
+		
+		//SSID
+		this.Ssid = (byte)((framePart[offset + 6]>>1) & 0xF);
+	}
+	/**
+	 * Create a byte array for the address field object
+	 * @param sourceAddress true indicates its user needs a source address
+	 * @return byte array 
+	 */
+	
+	public byte[] ToByteArray(boolean sourceAddress){
+        // Call Sign
+        byte[] tmp = new byte[7];
+        tmp[0] = (byte)(this.CallSign.charAt(0) << 1);
+        tmp[1] = (byte)(this.CallSign.charAt(1) << 1);
+        tmp[2] = (byte)(this.CallSign.charAt(2) << 1);
+        tmp[3] = (byte)(this.CallSign.charAt(3) << 1);
+        tmp[4] = (byte)(this.CallSign.charAt(4) << 1);
+        tmp[5] = (byte)(this.CallSign.charAt(5) << 1);
+
+        // SSID
+        tmp[6] = (byte)((this.Ssid << 1) | this.SSIDMask);
+        if (sourceAddress){
+            tmp[6] |= 0x01;
+        }
+
+        return tmp;
+   }
+
 	
 	
 }
