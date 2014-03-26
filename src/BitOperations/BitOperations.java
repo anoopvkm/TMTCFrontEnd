9e04f011a8b1536b02a8e92a13ab063f476783e8
Anoop <anoopvkm@gmail.com>
2014-03-26 23:12:24 +0530

    Byte operations implemented

--- src/BitOperations/BitOperations.java
+++ src/BitOperations/BitOperations.java
@@ -1,5 +1,43 @@
 package BitOperations;
-
+/**
+ * This class deals with unsigned bianry operations
+ * @author Anoop R Santhosh
+ * @version 1.0
+ */
 public class BitOperations {
 
+	/**
+	 * Function for converting a byte to unsigned binary string
+	 * @param number The byte to be converted
+	 * @return String 
+	 */
+	public static String byteToBinaryString8(byte number){
+		if(number >= 0 && number <=127 ){
+			return "0"+Integer.toBinaryString(number);
+		}else{
+			return Integer.toBinaryString(number).substring(Integer.SIZE - Byte.SIZE);
+		}
+	}
+	
+	/**
+	 * Function for converting an integer (less than 256) to a unsigned byte
+	 * @param number the number to be converted
+	 * @return byte
+	 */
+	public static byte IntegerToUnsignedbyte8(int number){
+		String Trep = byteToBinaryString8((byte)number);
+		int Tint = Integer.valueOf(Trep, 2);
+		return (byte)Tint;
+
+	}
+	
+	/**
+	 * Function for converting a 8 bit unsigned byte to an integer
+	 * @param number
+	 * @return integer
+	 */
+	public static int UnsignedBytetoInteger8(byte number){
+		String Trep = byteToBinaryString8(number);
+		return Integer.valueOf(Trep, 2);
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,15 @@
+import BitOperations.BitOperations;
+
 
 public class Test {
 
 	public static void main (String [] args){
 	
-		byte b1 = (byte)121;
+		byte b1 = (byte)255;
 	
 		
-		System.out.println(Integer.toBinaryString(b1));
+		System.out.println(BitOperations.IntegerToUnsignedbyte8(b1));
+		byte b2 = BitOperations.IntegerToUnsignedbyte8(b1);
+		System.out.println(BitOperations.UnsignedBytetoInteger8(b2));
 	}
 }
