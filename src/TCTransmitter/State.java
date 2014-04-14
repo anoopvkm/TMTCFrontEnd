9bab0faa3d83c80509012b5a67c4ab3b90dd04e5
Anoop <anoopvkm@gmail.com>
2014-04-14 10:29:12 +0530

    TC Transmitter progress

--- src/TCTransmitter/State.java
+++ src/TCTransmitter/State.java
@@ -3,5 +3,6 @@ package TCTransmitter;
 public enum State {
 
 	READY,
-	WAIT_FOR_ACK
+	WAIT_FOR_ACK,
+	WAIT_FOR_TRANS
 }
--- src/TCTransmitter/TCTransmitter.java
+++ src/TCTransmitter/TCTransmitter.java
@@ -6,6 +6,7 @@ import java.util.Queue;
 
 import AX25.AX25AddressField;
 import AX25.AX25Telecommand;
+import BitOperations.BitOperations;
 import TMTCFrontEnd.ApplicationData;
 
 /**
@@ -25,18 +26,22 @@ public class TCTransmitter {
 	private LinkedList<AX25Telecommand> ResendPacketQueue;
 	
 	// outstanding Packet queue
-	private LinkedList<AX25Telecommand> OutStandingPacketQueue;
+	private HashMap<Integer,AX25Telecommand> OutStandingPacketMap;
 	
 	// HashMap to store sendcounter
-	HashMap<Integer,Integer> SendCounter;
+	private HashMap<Integer,Integer> SendCounter;
 	
 	// the current ACK to be transmitted
-	AX25Telecommand currentAck;
+	private AX25Telecommand currentAck;
+	
 	// Enum representing the state of transmitter
-	State state;
+	private State state;
+	
+	// boolean variable true - indicates transmitter is on 
+	private boolean TransmitterState;
 	
 	// Variable to keep track of frame count
-	int frameCounter;
+	private int frameCounter;
 	
 	// constructor
 	public TCTransmitter(){
@@ -45,8 +50,9 @@ public class TCTransmitter {
 		frameCounter = 0;
 		EncodedPacketQueue = new LinkedList<AX25Telecommand>();
 		ResendPacketQueue = new LinkedList<AX25Telecommand>();
-		OutStandingPacketQueue = new LinkedList<AX25Telecommand>();
+		OutStandingPacketMap = new HashMap<Integer,AX25Telecommand>();
 		SendCounter = new HashMap<Integer,Integer>();
+		TransmitterState = false;
 	}
 	
 	/*
@@ -88,6 +94,7 @@ public class TCTransmitter {
 		switch(state){
 			case READY : dispatchPackets();
 						 break; 
+			default    : // TODO a trace statement 
 		}
 	}
 	/**
@@ -98,38 +105,77 @@ public class TCTransmitter {
 	}
 	/**
 	 * Function to be called in case of negative ACK
-	 * 
+	 * TODO indicate somewhere that packet was send ???
 	 */
-	public void receiveNegativeAck(){
+	public void receiveAck(byte [] acks){
+
+		switch(state){
+			case WAIT_FOR_ACK : 	for(int i =0;i<acks.length;i++){
+										int counter = BitOperations.UnsignedBytetoInteger8(acks[i]);
+										if(OutStandingPacketMap.containsKey(counter)){
+											OutStandingPacketMap.remove(counter);
+										}
+									}
+									ResendPacketQueue.clear();
+									ResendPacketQueue.addAll(OutStandingPacketMap.values());
+									this.state = State.WAIT_FOR_TRANS;
+									break;
+									
+			default : // TODO trace;
+		
+		}
+	
 		
 	}
+	
 	/**
-	 * Function to be called all packets are received
+	 * Function to indicate transmitter is turned on
 	 */
-	public void allReceived(){
-		
+	public void TransmitterON(){
+		this.TransmitterState = true;
 	}
+	
+	/**
+	 * Function to indicate transmitter is turned off
+	 */
+	public void TransmitterOFF(){
+		this.TransmitterState = false;
+	}
+	
 	/**
 	 * Function to dispatch all AX.25 packets
 	 */
 	private void dispatchPackets(){
-		// send ack first
-		dropToSocket(currentAck.ToByteArray());
-		
-		// resend packets
-		for(int i =0;i<ResendPacketQueue.size();i++){
-			dropToSocket(ResendPacketQueue.get(i).ToByteArray());
-			SendCounter.get(ResendPacketQueue.get(i).GetCounter())++;
+		if(TransmitterState){
+			// send ack first
+			dropToSocket(currentAck.ToByteArray());
+			
+			// resend packets
+			for(int i =0;i<ResendPacketQueue.size();i++){
+			
+				int Tcounter = ResendPacketQueue.get(i).GetCounter();
+				int Val = SendCounter.get(Tcounter);
+				SendCounter.remove(Tcounter);
+				Val++;
+				SendCounter.put(Tcounter, Val);
+				OutStandingPacketMap.put(Tcounter, ResendPacketQueue.get(i));
+				dropToSocket(ResendPacketQueue.get(i).ToByteArray());
+			}
+	
+			ResendPacketQueue.clear();
+			
+			// sending new packets
+			for(int i =0;i<EncodedPacketQueue.size();i++){
+	
+				SendCounter.put(EncodedPacketQueue.get(i).GetCounter(), 1);
+				OutStandingPacketMap.put(EncodedPacketQueue.get(i).GetCounter(), EncodedPacketQueue.get(i));
+				dropToSocket(EncodedPacketQueue.get(i).ToByteArray());
+			}
+			
+			EncodedPacketQueue.clear();
+			
+			this.state = State.WAIT_FOR_ACK;
 		}
-		OutStandingPacketQueue.addAll(ResendPacketQueue);
-		ResendPacketQueue.clear();
-		
-		// sending new packets
-		for(int i =0;i<EncodedPacketQueue.size();i++){
-			dropToSocket(EncodedPacketQueue.get(i).ToByteArray());
-		}
-		OutStandingPacketQueue.addAll(EncodedPacketQueue);
-		EncodedPacketQueue.clear();
 	}
 	
 	/**
