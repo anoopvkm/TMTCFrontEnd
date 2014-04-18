f821a620ec68a0c67f760b05d5f9963f892c1a2b
Anoop <anoopvkm@gmail.com>
2014-04-18 08:58:00 +0530

    SQL client

--- src/SQLClient/SQLClient.java
+++ src/SQLClient/SQLClient.java
@@ -1,6 +1,96 @@
 package SQLClient;
 
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.LinkedList;
+
+import javax.sql.rowset.serial.SerialBlob;
+
+
+import AX25.AX25Telecommand;
+import AX25.AX25Telemetry;
+
 public class SQLClient {
 
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost/TMTCFrontEnd";
+
+	   //  Database credentials
+	static final String USER = "root";
+	static final String PASS = "ROCKGANA89";
 	
+	/**
+	 * Function to archieve Telemtery packets
+	 * Writes to AX
+	 * @param arcList
+	 * @throws SQLException
+	 */
+	public void ArchieveTelemetry(LinkedList<AX25Telemetry> arcList) throws SQLException{
+		  Connection conn = null;
+		  Statement stmt = null;
+		  
+		  try {
+			 
+			  Class.forName("com.mysql.jdbc.Driver");
+			  
+			  conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			 
+			  stmt = conn.createStatement();
+			  
+			  System.out.println(arcList.size());
+			  for(int i = 0;i<arcList.size();i++){
+				  Date date = new Date();
+				  Timestamp ts = new Timestamp(date.getTime());
+				  Blob b = new SerialBlob(arcList.get(i).ToByteArray());
+				//  b.setBytes(1, arcList.get(i).ToByteArray());
+				  
+				  String query =  "INSERT INTO AX25Telemetry (TimeStamp , Frame ) VALUES ('"+ts +"','"+  b + "') ";
+				  stmt.executeUpdate(query);
+				
+			  }
+		     
+			  conn.close();
+		     
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void ArchieveTeleCommand(LinkedList<AX25Telecommand> arcList) throws SQLException{
+		  Connection conn = null;
+		  Statement stmt = null;
+		  
+		  try {
+			 
+			  Class.forName("com.mysql.jdbc.Driver");
+			  
+			  conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			 
+			  stmt = conn.createStatement();
+			  
+			  System.out.println(arcList.size());
+			  for(int i = 0;i<arcList.size();i++){
+				  Date date = new Date();
+				  Timestamp ts = new Timestamp(date.getTime());
+				  Blob b = new SerialBlob(arcList.get(i).ToByteArray());
+				//  b.setBytes(1, arcList.get(i).ToByteArray());
+				  
+				  String query =  "INSERT INTO AX25Telecommand (TimeStamp , Frame ) VALUES ('"+ts +"','"+  b + "') ";
+				  stmt.executeUpdate(query);
+				
+			  }
+		     
+			  conn.close();
+		     
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,45 +1,24 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+import java.util.LinkedList;
 
+import SQLClient.SQLClient;
 import Trace.Trace;
+import AX25.AX25Telemetry;
 import BitOperations.BitOperations;
 
 
 public class Test {
 
-	  public static void main(String a[]) {
+	  public static void main(String a[]) throws SQLException {
 
-	        Thread addthread = new Thread() {
-	                public void run() {
-	                	for(int i =0;i<20;i++){
-	                		System.out.println("a");
-	                		try {
-								sleep(5);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-	                	}
-	                	
-	            }
-	        };
-	        Thread subtractThread = new Thread() {
-	              public void run() {
-
-	              for(int i =0;i<20;i++){
-	            	  System.out.println("b");
-	            		try {
-							sleep(4);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-	              }
-
-	            }
-	        };
-	        addthread .start();
-	        subtractThread .start();
-	    }
+		  AX25Telemetry bb = new AX25Telemetry();
+		  LinkedList<AX25Telemetry> bbbb = new LinkedList<AX25Telemetry>();
+		  bbbb.add(bb);
+		  SQLClient sql = new SQLClient();
+		 
+		  sql.ArchieveTelemetry(bbbb);
+	  }
 }
